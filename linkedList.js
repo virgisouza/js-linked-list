b09f8282773ed814322a37c799d49ed42390b0ca
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-14 14:07:04 -1000

    created all functions, currently working on add() method

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,67 @@
 /**
  * @name  linkedListGenerator
  * @description  Main Module
- * @return {Object} an object exposing methods to be used to manipulate a linked list
+ * @return {Object} an object exposing methods to be used to manipulate a linked list\
+
+ GOAL
+    {
+      value: 'Ready Player One',
+      next: {
+        value: '1982',
+        next: {
+          value: 'Neuromancer',
+          next: {
+            value: 'Snow Crash',
+            next: null
+          }
+        }
+      }
+    };
+
+
  */
 function linkedListGenerator(){
+  var head = null;
+
+
+  function getHead() {
+    return head;
+  }
+  function getTail() {
+    return head;
+  }
+  function add(newLink) {
+    newNodeObj = {
+      value : 'Ready Player One',
+      next : newNodePointer };
+    if(newLink === null){
+
+      newNodeObj.length++;
+    }else{
+      //don't add a node
+    }
+
+
+
+    return ;
+  }
+  function remove() {
+
+  }
+  function get() {
+
+  }
+  function insert() {
+
+  }
+
+  return {
+   getHead : getHead,
+   getTail : getTail,
+   add : add,
+   remove : remove,
+   get : get,
+   insert : insert
+  }
 
 }
